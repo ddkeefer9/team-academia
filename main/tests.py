b536582deb99eae310343cd3f8fbc279105efa15
unknown <kkersten@unomaha.edu>
2022-02-24 15:29:56 -0600

    url test does not exist and admin exists

--- main/tests.py
+++ main/tests.py
@@ -8,3 +8,11 @@ class BasicTests(TestCase):
 	def test_view_url_exists_at_desired_location(self):
 		response = self.client.get('/')
 		self.assertEqual(response.status_code, 200)
+
+	def test_url_does_not_exist(self):
+		response = self.client.get('/fake_site')
+		self.assertEqual(response.status_code, 404)
+
+	def test_url_admin(self):
+		response = self.client.get('/admin')
+		self.assertEqual(response.status_code, 301)
