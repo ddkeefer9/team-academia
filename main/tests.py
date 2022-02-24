98f6c65247d47d0698013a24f6c1bd16a2e58975
Connor Peterson <cpeterson53@unomaha.edu>
2022-02-24 15:13:49 -0600

    Added Basic Test

--- main/tests.py
+++ main/tests.py
@@ -1,6 +1,10 @@
 from django.test import TestCase
 import pytest
 # Create your tests here.
+class BasicTests(TestCase):
+	def test_example(self):
+		assert 1 == 1
 
-def test_example():
-	assert 1 == 1
+	def test_view_url_exists_at_desired_location(self):
+		response = self.client.get('/')
+		self.assertEqual(response.status_code, 200)
