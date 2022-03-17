f9826ce92880aa186742c10d687dcb8f7c8be711
cpeterson406 <cpeterson406@gmail.com>
2022-03-17 15:35:08 -0500

    Updated Departments Dropdown

--- main/views.py
+++ main/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import MakereportsSloinreport
 from .models import MakereportsDegreeprogram
+from .models import MakereportsDepartment
 # Create your views here.
 
 def index(request):
         # 
+        departments = MakereportsDepartment.objects.all()
         degreePrograms = MakereportsDegreeprogram.objects.all()
         return render(
                 request, 
                 'home/home_page.html',
-                {'showdegrees':degreePrograms},
+                {'showDepartments':departments,
+                'showDegrees':degreePrograms},
         )
