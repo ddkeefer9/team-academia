7b0308a4ed81311c8d085ba86a1778a414c24fee
ddkeefer9 <ddkeefer9@gmail.com>
2022-04-04 18:52:01 -0500

    Initial degree comparison pdf generation

--- main/urls.py
+++ main/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path("", basic_views.HomePage.display_index, name="home"),
     path("smartAssistant", smart_view.SmartAssistantPage.display_smartAssistant, name="smartAssistant"),
     path("pdfGen", reports_view.PDFPage.display_pdfGen, name="pdfGen"),
+    path("pdfDegreeGen", reports_view.PDFPage.display_degree_pdfGen, name="pdfDegreeGen"),
     path("degreeDropdown", basic_views.HomePage.display_sendDegrees, name="degreeDropdown"),
     path("historical", historical_view.HistoricalPage.display_historical, name="historical")
 ]
