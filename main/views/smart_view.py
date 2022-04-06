c5ca93202c7df0310f42ec549f17b448ac3e521e
cpeterson406 <cpeterson406@gmail.com>
2022-04-06 16:57:28 -0500

    Added SLO text generation

--- main/views/smart_view.py
+++ main/views/smart_view.py
@@ -4,37 +4,44 @@ from main.models import \
         MakereportsReport, MakereportsSloinreport, MakereportsDegreeprogram, MakereportsDepartment, MakereportsSlostatus, \
         MakereportsSlostatus 
 from AcademicAssessmentAssistant.settings import BASE_DIR
+from .util.pdfgenhelpers import PDFGenHelpers as pg
+from .util.smartAssistantHelper import SmartAssistantHelper as sa
 # Report Gen Imports
 from django.http import FileResponse
 import io
 # Create your views here.
 class SmartAssistantPage():
-    """
-    Smart Assistant View
-    """
-    def display_smartAssistant(request):        
-        departments = MakereportsDepartment.objects.all()
-        degreePrograms = MakereportsDegreeprogram.objects.all()
+	"""
+	Smart Assistant View
+	"""
+	def display_smartAssistant(request):        
+		departments = MakereportsDepartment.objects.all()
+		degreePrograms = MakereportsDegreeprogram.objects.all()
 
-        if request.method == "POST" and "department" in request.POST and "degree-program" in request.POST:
-            start_department = MakereportsDepartment.objects.filter(id=int(request.POST['department']))[0]
-            start_degree_program = MakereportsDegreeprogram.objects.filter(name=request.POST['degree-program'])[0]
-            return render(
-                request, 
-                'smart_assistant/smart_assistant.html',
-                {
-                        'showDepartments':departments,
-                        'showDegrees':degreePrograms,
-                        'start_department': start_department,
-                        'start_degree_program': start_degree_program
-                }       
-            )
 
-        return render(
-        request, 
-        'smart_assistant/smart_assistant.html',
+		if request.method == "POST" and "department" in request.POST and "degree-program" in request.POST:
+			start_department = MakereportsDepartment.objects.filter(id=int(request.POST['department']))[0]
+			start_degree_program = MakereportsDegreeprogram.objects.filter(name=request.POST['degree-program'])[0]
+			dprqs, sirqs, sirsqs = pg.pdfGenQuery(request.POST['degree-program'])
+			slo_texts = sa.SLOList_goaltext(sirqs)
+			slos = [(element, "FEEDBACK_PLACEHOLDER") for element in slo_texts]
+			return render(
+				request, 
+				'smart_assistant/smart_assistant.html',
+				{
+						'showDepartments':departments,
+						'showDegrees':degreePrograms,
+						'start_department': start_department,
+						'start_degree_program': start_degree_program,
+						'showSLOs':slos
+				}       
+			)
+
+		return render(
+			request, 
+			'smart_assistant/smart_assistant.html',
 			{
 				'showDepartments':departments,
 				'showDegrees':degreePrograms,
 			}       
-        )
+		)
---
+++ main/views/util/smartAssistantHelper.py
@@ -0,0 +1,31 @@
+
+from AcademicAssessmentAssistant.settings import BASE_DIR
+import re
+from .pdfgenhelpers import PDFGenHelpers as pg
+from ...models import \
+        MakereportsAssessmentdata, MakereportsAssessmentversion, MakereportsReport, MakereportsSloinreport, MakereportsDegreeprogram, MakereportsDepartment, MakereportsSlostatus
+
+class SmartAssistantHelper():
+
+    """
+            A 3-tuple of the form: (dpqs, sirqs, sirsqs) where:
+                - dpqs: is the Degree program queryset.
+                - sirqs: is the SLOinreport queryset.
+                - sirsqs: is the SLOinreportstatus queryset.
+    """
+    def SLOList_goaltext(slo_list):
+        slo_texts = []
+        for i in range(len(slo_list)):
+            slo_texts.append(SmartAssistantHelper.SLO_goaltext(slo_list[i]))
+        return slo_texts
+
+    def SLO_goaltext(slo_object):
+        first_report_goal_text = ""
+        if slo_object is not None:
+            first_report_goal_text = SmartAssistantHelper.cleanhtml(slo_object.goaltext)
+        return first_report_goal_text
+
+
+    def cleanhtml(raw_html):
+        return re.sub(re.compile('<.*?>'), '', raw_html)
+
