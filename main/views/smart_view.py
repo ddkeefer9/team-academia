a44c8bf87856ea39390eb536303423606a5036b1
cpeterson406 <cpeterson406@gmail.com>
2022-04-06 13:01:03 -0500

    Dropdowns for Smart Assistant View

--- main/views/smart_view.py
+++ main/views/smart_view.py
@@ -12,8 +12,29 @@ class SmartAssistantPage():
     """
     Smart Assistant View
     """
-    def display_smartAssistant(request):
+    def display_smartAssistant(request):        
+        departments = MakereportsDepartment.objects.all()
+        degreePrograms = MakereportsDegreeprogram.objects.all()
+
+        if request.method == "POST" and "department" in request.POST and "degree-program" in request.POST:
+            start_department = MakereportsDepartment.objects.filter(id=int(request.POST['department']))[0]
+            start_degree_program = MakereportsDegreeprogram.objects.filter(name=request.POST['degree-program'])[0]
             return render(
-                    request, 
-                    'smart_assistant/smart_assistant.html',
+                request, 
+                'smart_assistant/smart_assistant.html',
+                {
+                        'showDepartments':departments,
+                        'showDegrees':degreePrograms,
+                        'start_department': start_department,
+                        'start_degree_program': start_degree_program
+                }       
             )
+
+        return render(
+        request, 
+        'smart_assistant/smart_assistant.html',
+			{
+				'showDepartments':departments,
+				'showDegrees':degreePrograms,
+			}       
+        )
