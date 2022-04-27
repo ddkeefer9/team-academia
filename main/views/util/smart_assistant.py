85a8023981d86b3e36d9e408b84669c50cf05dd8
cpeterson406 <cpeterson406@gmail.com>
2022-04-26 22:46:49 -0500

    Aggregate Feedback and Color Scoring

--- main/views/util/smart_assistant.py
+++ main/views/util/smart_assistant.py
@@ -4,6 +4,7 @@ import re
 import nltk
 import json
 from nltk.tokenize import word_tokenize
+from django.utils.html import format_html
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet as wn
 from .pdf_generation import PDFGenHelpers as pg
@@ -17,7 +18,10 @@ class SLO_Object():
         self.slo_object = slo_object
         self.goal_text = SmartAssistantHelper.SLO_goaltext(self.slo_object)
         self.bloom_taxonomy = SmartAssistantHelper.SLO_Blooms(self.slo_object)
+        self.bloom_score = SmartAssistantHelper.Blooms_Score[self.bloom_taxonomy]
+        self.count = SmartAssistantHelper.discipline_count(self)
         self.feedback = SmartAssistantHelper.feedback(self)
+        self.score = SmartAssistantHelper.scoreSLO(self)
 
 class Degree_Object():
     def __init__(self, degree):
@@ -25,6 +29,10 @@ class Degree_Object():
         self.keywords = SmartAssistantHelper.keywords[self.degree_name]
         self.keys = SmartAssistantHelper.keys_from_keywords(self.keywords)
         self.level = degree.level
+        self.levelText = "undergraduate" if degree.level == "UG" else "graduate"
+        self.bloom_level = 3 if degree.level == "UG" else 5
+        self.bloom_level_text = "Application" if degree.level == "UG" else "Synthesis"
+
 
 class SmartAssistantHelper():
     f = open('main/views/util/degree_keywords.json', 'r')
@@ -68,27 +76,75 @@ class SmartAssistantHelper():
 
     def feedback(SLO):
         feedback = ""
+        if SLO.count >= 1:
+            feedback += "SLO contains discipline specific content. It contains "+ str(SLO.count) + (" mentions " if SLO.count > 1 else " mention ") + "to relevant keyword synonyms for " + SLO.degree.degree_name + " in WordNet. "
+        else:
+            feedback += "SLO is generic and does not contain discipline specific content for relevant keyword synonyms for " + SLO.degree.degree_name + " in WordNet. "
+            feedback += "Consider updating the SLO to contain objectives more relevant to the discipline. "
+
+        feedback += "SLO reaches Bloom's Taxonomy level of " + SLO.bloom_taxonomy + ". "
+        if SmartAssistantHelper.Blooms_Score[SLO.bloom_taxonomy] >= 3:
+            bloom_num = SmartAssistantHelper.Blooms_Score[SLO.bloom_taxonomy] - SmartAssistantHelper.Blooms_Score[SmartAssistantHelper.Blooms_Abbrev['AP']]
+            feedback += "This SLO is " + str(bloom_num) + (" levels " if bloom_num > 1 or bloom_num == 0 else " level ") + " ahead of the required minimum. "
+        else:
+            bloom_num = SmartAssistantHelper.Blooms_Score[SmartAssistantHelper.Blooms_Abbrev['AP']] - SmartAssistantHelper.Blooms_Score[SLO.bloom_taxonomy]
+            feedback += "This SLO is " + str(bloom_num) + (" level " if bloom_num >1 or bloom_num ==0 else " level ") + " behind the required minimum. "
+        return feedback
+
+    def scoreSLO(SLO):
+        score = 0 
+        if(SLO.bloom_score >= SLO.degree.bloom_level):
+            score += 1
+        if(SLO.count > 0):
+            score += 1
+        return score
+            
+
+
+    def discipline_count(SLO):
         tokens = SmartAssistantHelper.prepare_text_for_lda(SLO.goal_text)
         count = 0
         for token in tokens:
             if token in SLO.degree.keys:
                 count = count + 1
-        
-        if count >= 1:
-            feedback += "SLO contains discipline specific content. It contains "+ str(count) + (" mentions " if count > 1 else " mention ") + "to relevant keyword synonyms for " + SLO.degree.degree_name + " in WordNet. "
-        else:
-            feedback += "SLO is generic and does not contain discipline specific content for relevant keyword synonyms for " + SLO.degree.degree_name + " in WordNet. "
-            feedback += "Consider updating the SLO to contain objectives more relevant to the discipline. "
+        return count
 
-        feedback += "SLO reaches Bloom's Taxonomy level of " + SLO.bloom_taxonomy + ". A degree of LEVEL must contain atleast one SLO with a Bloom's Taxonomy level of atleast " + SmartAssistantHelper.Blooms_Abbrev['AP'] + ". "
-        if SmartAssistantHelper.Blooms_Score[SLO.bloom_taxonomy] >= 3:
-            bloom_num = SmartAssistantHelper.Blooms_Score[SLO.bloom_taxonomy] - SmartAssistantHelper.Blooms_Score[SmartAssistantHelper.Blooms_Abbrev['AP']]
-            feedback += "This degree is " + str(bloom_num) + (" levels " if bloom_num > 1 or bloom_num == 0 else " level ") + " ahead of the required minimum. "
-        else:
-            bloom_num = SmartAssistantHelper.Blooms_Score[SmartAssistantHelper.Blooms_Abbrev['AP']] - SmartAssistantHelper.Blooms_Score[SLO.bloom_taxonomy]
-            feedback += "This degree is " + str(bloom_num) + (" level " if bloom_num >1 or bloom_num ==0 else " level ") + " behind the required minimum. "
-        return feedback
 
+    def aggregateFeedbackMetrics(SLOList):
+        #Each index is a level of Bloom's Taxonomy
+        degree = SLOList[0].degree
+        numSLO = 0
+        bloom_count = 0
+        bloom_list = [0,0,0,0,0,0]
+        for idx,SLO in enumerate(SLOList):
+            if SLO.bloom_score >= degree.bloom_level:
+                bloom_count += 1
+            if SLO.count > 0:
+                numSLO += 1
+            bloom_list[SLO.bloom_score] += 1
+        return (numSLO, bloom_count, bloom_list)
+            
+            
+    def aggregateFeedbackText(SLOList):
+        degree = SLOList[0].degree
+        (numSLO, bloom_count, bloom_list) = SmartAssistantHelper.aggregateFeedbackMetrics(SLOList)
+        score = 0
+        if numSLO > len(SLOList) / 2:
+            score += 1
+        if bloom_count > 1:
+            score += 1
+        bloom_levels = list(SmartAssistantHelper.Blooms_Abbrev.values())
+        feedback = ""
+        feedback = "A " + degree.level +" degree must contain atleast one SLO with a Bloom's Taxonomy level of atleast " +  degree.bloom_level_text + " status. " 
+        feedback += "This degree program contains " + str(bloom_count) + " SLO's that match or exceed the minimum requirement. "
+        feedback += " A degree should consist of atleast half (" + str(int(len(SLOList)/2)) + ") of the SLO's considered discipline specific. "
+        feedback += "This degree contains " + str(numSLO) + (" SLO " if numSLO == 1 else " SLOs ") + " considered discipline specific. "
+        feedback += " The following is the distribution of SLO's which match each Bloom's Taxonomy Level.<br><br>Bloom Distribution:<br>"
+        for idx,element in enumerate(bloom_list):
+            feedback += bloom_levels[idx] + ": " + str(element) + "<br>"
+
+        feedback = format_html("<p>"+ feedback +"</p>")
+        return (feedback, score)
 
     def keys_from_keywords(keywords):
         key_syms = set()
